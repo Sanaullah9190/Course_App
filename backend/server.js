4c4a96910a0fd408e63e42eba9f0c1589d41b8ce
sanaullah ansari <sanaullaht.social@gmail.com>
2026-02-21 10:27:18 +0530

    add frontend link in backend origin corse

--- backend/server.js
+++ backend/server.js
@@ -20,14 +20,18 @@ cloudinary.config({
 });
 
 // 2. Middlewares
-const corsOptions = {
-    origin: '*https://course-app-frontend-7evw.onrender.com', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-};
+// const corsOptions = {
+//     origin: '*https://course-app-frontend-7evw.onrender.com', 
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use(cors({
+  origin: "https://course-app-frontend-7evw.onrender.com"
+}));
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
