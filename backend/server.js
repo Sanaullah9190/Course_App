5a1b0066d1d93dea4e78a581d1cc831e8e22bc49
sanaullah ansari <sanaullaht.social@gmail.com>
2026-02-21 12:05:59 +0530

    hh

--- backend/server.js
+++ backend/server.js
@@ -20,18 +20,18 @@ cloudinary.config({
 });
 
 // 2. Middlewares
-// const corsOptions = {
-//     origin: '*https://course-app-frontend-7evw.onrender.com', 
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     credentials: true
-// };
+const corsOptions = {
+    origin: '*https://course-app-frontend-7evw.onrender.com', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
-app.use(cors({
-  origin: "*"
-}));
+// app.use(cors({
+//   origin: "*"
+// }));
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
