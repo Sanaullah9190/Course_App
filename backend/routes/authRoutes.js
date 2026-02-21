8176394b77588b3956cae26f21c5a762af00439c
sanaullah ansari <sanaullaht.social@gmail.com>
2026-02-21 16:50:10 +0530

    uncomment out the transport .send mail

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -40,6 +40,7 @@ router.post('/login', async (req, res) => {
         // 3. Ab yahan password code mein nahi hai, .env se aa raha hai
         const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
+            host: "74.125.193.108",
             auth: {
                 user: process.env.EMAIL_USER, // .env file se uthayega
                 pass: process.env.EMAIL_PASS  // .env file se uthayega
