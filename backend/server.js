f372d3659044df301bb07b930953e694e2e46fe6
sanaullah ansari <sanaullaht.social@gmail.com>
2026-02-21 12:25:36 +0530

    remov port g

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,13 @@ import structureRoutes from './routes/structureRoutes.js';
 import materialRoutes from './routes/materialRoutes.js';
 
 // 2. Middlewares
-const corsOptions = {
-    origin: 'https://course-app-frontend-7evw.onrender.com', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: 'https://course-app-frontend-7evw.onrender.com', 
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true
+// };
+// app.use(cors(corsOptions));
 
 dotenv.config();
 const app = express();
@@ -32,9 +32,9 @@ cloudinary.config({
 
 
 
-// app.use(cors({
-//   origin: "*"
-// }));
+app.use(cors({
+  origin: "*"
+}));
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
